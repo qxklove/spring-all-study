ca58a733f43e089df9a3f60c1b284acfcc1aed15
qklove <1059679161@qq.com>
2021-07-28 00:27:56 +0800

    h2 数据源配置

--- spring-boot-datasource/src/main/java/com/qxk/springall/springbootdatasource/SpringBootDatasourceApplication.java
+++ spring-boot-datasource/src/main/java/com/qxk/springall/springbootdatasource/SpringBootDatasourceApplication.java
@@ -1,13 +1,43 @@
 package com.qxk.springall.springbootdatasource;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
-public class SpringBootDatasourceApplication {
+@Slf4j
+public class SpringBootDatasourceApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDatasourceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection connection = dataSource.getConnection();
+        log.info(connection.toString());
+        connection.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("select * from FOO").forEach(row -> log.info(row.toString()));
+    }
 }
