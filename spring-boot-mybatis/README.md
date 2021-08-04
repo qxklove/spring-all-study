e02a452a22fa09205f8220d906ca996ae2b41074
qxklove <1059679161@qq.com>
2021-08-05 01:07:23 +0800

    Mybatis generator

--- spring-boot-mybatis/README.md
+++ spring-boot-mybatis/README.md
@@ -1,4 +1,5 @@
-# Spring Boot Mybatis
+# Spring Boot MyBatis
+作为子项目运行有问题，可以拿出来作为单独项目运行
 
 ## 一些简单配置
 * mybatis.mapper-locations = classpath*:mapper/**/\*.xml 指定Mybatis的Mapper文件
@@ -10,3 +11,25 @@
 * @MapperScan配置扫描位置
 * @Mapper定义接口
 * 映射的定义 -- XML与注解
+
+## 让MyBatis更好用的工具
+### [MyBatis Generator](http://mybatis.org/generator/)
+根据数据库表生成相关代码 POJO，Mapper接口，SQL Map XML
+
+本项目中用 `generatorConfig.xml` 配置文件来生成相关代码，主要配置了：
+* jdbcConnection
+* javaModelGenerator
+* sqlMapGenerator
+* javaClientGenerator（ANNOTATEDMAPPER/XMLMAPPER/MIXEDMAPPER）
+* table
+
+生成时可以用的一些插件：
+* FluentBuilderMethodsPlugin：类似lombok的Builder
+* ToStringPlugin
+* SerializablePlugin
+* RowBoundsPlugin：分页用到
+
+使用生成对象
+* 简单操作，直接使用生成的xxxMapper的方法
+* 复杂查询，使用生成的xxxExample对象
+
--- spring-boot-mybatis/src/main/java/com/qxk/springall/springbootmybatis/SpringBootMybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/qxk/springall/springbootmybatis/SpringBootMybatisApplication.java
@@ -1,10 +1,17 @@
 package com.qxk.springall.springbootmybatis;
 
 import com.qxk.springall.springbootmybatis.mapper.CoffeeMapper;
+import com.qxk.springall.springbootmybatis.mapper.CoffeeWithMyBatisGeneratorMapper;
 import com.qxk.springall.springbootmybatis.model.Coffee;
+import com.qxk.springall.springbootmybatis.model.CoffeeWithMyBatisGenerator;
+import com.qxk.springall.springbootmybatis.model.CoffeeWithMyBatisGeneratorExample;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -12,12 +19,18 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Slf4j
 @MapperScan("com.qxk.springall.springbootmybatis.mapper")
 @SpringBootApplication
 public class SpringBootMybatisApplication implements ApplicationRunner {
     @Autowired
     private CoffeeMapper coffeeMapper;
+    @Autowired
+    private CoffeeWithMyBatisGeneratorMapper coffeeWithMyBatisGeneratorMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMybatisApplication.class, args);
@@ -25,6 +38,12 @@ public class SpringBootMybatisApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        testWithoutMyBatisGenerator();
+        //generateArtifacts();
+        playWithArtifacts();
+    }
+
+    private void testWithoutMyBatisGenerator() {
         Coffee c = Coffee.builder().name("espresso")
                 .price(Money.of(CurrencyUnit.of("CNY"), 20.0)).build();
         int count = coffeeMapper.save(c);
@@ -37,5 +56,39 @@ public class SpringBootMybatisApplication implements ApplicationRunner {
 
         c = coffeeMapper.findById(c.getId());
         log.info("Find Coffee: {}", c);
+    }
+
+    private void generateArtifacts() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(
+                this.getClass().getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    private void playWithArtifacts() {
+        CoffeeWithMyBatisGenerator espresso = new CoffeeWithMyBatisGenerator()
+                .withName("espresso")
+                .withPrice(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .withCreateTime(new Date())
+                .withUpdateTime(new Date());
+        coffeeWithMyBatisGeneratorMapper.insert(espresso);
+
+        CoffeeWithMyBatisGenerator latte = new CoffeeWithMyBatisGenerator()
+                .withName("latte")
+                .withPrice(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .withCreateTime(new Date())
+                .withUpdateTime(new Date());
+        coffeeWithMyBatisGeneratorMapper.insert(latte);
+
+        CoffeeWithMyBatisGenerator s = coffeeWithMyBatisGeneratorMapper.selectByPrimaryKey(1L);
+        log.info("CoffeeWithMyBatisGenerator {}", s);
+
+        CoffeeWithMyBatisGeneratorExample example = new CoffeeWithMyBatisGeneratorExample();
+        example.createCriteria().andNameEqualTo("latte");
+        List<CoffeeWithMyBatisGenerator> list = coffeeWithMyBatisGeneratorMapper.selectByExample(example);
+        list.forEach(e -> log.info("selectByExample: {}", e));
     }
 }
